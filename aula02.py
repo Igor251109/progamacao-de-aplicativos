f9d4849d25abed1d0775ae9f0665ee3846c73650
Igor Oliveira <igorrocha2511@gmail.com>
2026-02-18 12:44:01 -0300

    primeira aula de progamação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 1 # variável int
+numero_dividido = 10.56 # variável float
+eu_to_tentando = True # variável booleana
+palavra = "segunda tentativa" # variável de texto
+
+
+print(numero)
+print(numero_dividido)
+print(eu_to_tentando)
+print(palavra)
