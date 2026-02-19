16edce06d374d4f5a7cdd6ee852024515fb7b78f
Igor Oliveira Rocha <igorrocha2511@gmail.com>
2026-02-19 10:12:39 -0300

    animal.py

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "cachorro" # variável string
+idade = 5 # variável int
+peso = 35 # variável float 
+sexo = "masculino" # variável string
+
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
