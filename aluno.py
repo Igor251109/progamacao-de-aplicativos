5f67572f55481d7f1b8848e253745ad4a763c24f
igor Oliveira <igorrocha2511@gmail.com>
2026-02-20 09:51:46 -0300

    lalala

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = input ("diga seu nome: ")
+idade = int(input ("diga sua idade: "))
+telefone = int(input("diga seu telefone: "))
+cidade = input("diga sua cidade: ")
+
+print("olá", nome)
+print("sua idade é", idade)
+print("seu telefone é", telefone)
+print("você mora em", cidade)
---
+++ matematica.py
@@ -0,0 +1,12 @@
+numero1 = int (input ("digite o primeiro valor"))
+numero2 = int (input ("digite o segundo valor"))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1 / numero2
+multiplicacao = numero1 * numero2
+
+print("a soma é", soma)
+print("a subtração é", subtracao)
+print("a divisão é", divisao)
+print("a multiplicação é", multiplicacao)
