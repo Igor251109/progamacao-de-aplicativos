080b26caf69c850dfb2cfd09667f929f1a40dda4
igor <igorrocha2511@gmail.com>
2026-03-18 12:43:41 -0300

    18/03

---
+++ alistamento.py
@@ -0,0 +1,5 @@
+lista = ["ana", "bruno", "caio"]
+lista.append("daniel")
+lista.insert(0, "zuleica")
+
+print(lista)
---
+++ musica.py
@@ -0,0 +1,3 @@
+musica = ["musica 1", "musica 2", "musica 3", "musica 4", "musica 5"]
+musica.remove("musica 3")
+musica.pop[0]
