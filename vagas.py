37a1ff68bf5a2b05c9814a9d7726830ff78ac7f6
rafael <igorrocha2511@gmail.com>
2026-05-06 09:24:21 -0300

    06/05

--- vagas.py
+++ vagas.py
@@ -2,5 +2,5 @@ vaga = int(input("digite o numero de uma vaga de 0 a 3: "))
 
 vagas = ["livre", "ocupado", "livre", "ocupado"]
 
-if vaga % 12 == 0 and vagas =="livre":
+if vaga % 2 == 0 and vagas =="livre":
     print(f"vaga {vaga} liberada.")
