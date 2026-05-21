e1d82bf9a369cc6441884a0e4fddb5aee5ebbb0d
igor <igorrocha2511@gmail.com>
2026-05-20 21:02:53 -0300

    20|05

--- crud_json.py
+++ crud_json.py
@@ -13,7 +13,9 @@ def cadastrar_aluno():
              "nome": input("digite seu nome completo: "),
              "telefone": int(input("digite seu telefone: ")),
              "turma": int(input("qual sua turma?: ")),
-             "idade": int(input("digite sua idade: "))}
+             "idade": int(input("digite sua idade: "))
+             }
+    print('-' * 50)
 
     with open("crud.json", 'a') as arquivo:
         json.dump(dados, arquivo, indent=4, ensure_ascii=False)
@@ -32,3 +34,61 @@ def listar_aluno():
         n += 1
 
 
+def atualizar_aluno():
+    listar_aluno()
+    print('-' * 50)
+
+    qual_mudar = int(input('qual ID do aluno que quer mudar?: '))
+    print('-' * 50)
+
+    dados = {"cpf": int(input("digite seu CPF: ")),
+             "nome": input("digite seu nome completo: "),
+             "telefone": int(input("digite seu telefone: ")),
+             "turma": int(input("qual sua turma?: ")),
+             "idade": int(input("digite sua idade: "))
+             }
+    print('-' * 50)
+
+    with open("crud.json", 'r') as arquivo:
+        mudar = json.load(arquivo)
+
+        mudar[qual_mudar] = dados
+
+    with open("crud.json", 'w') as arquivo:
+        json.dumps(mudar)
+    
+    print('aluno atualizado com sucesso!')
+
+
+def excluir_aluno():
+    listar_aluno()
+    print('-' * 50)
+
+    qual_excluir = int(input('qual ID do aluno que quer excluir?: '))
+    print('-' * 50)
+
+    with open("crud.json", 'r') as arquivo:
+        excluir = json.load(arquivo)
+
+        del excluir[qual_excluir]
+    
+    with open("crud.json", 'w') as arquivo:
+        json.dumps(excluir)
+
+    print("aluno excluido com sucesso!")
+
+
+while True:
+    print('-' * 50)
+    print("1 - adicionar aluno | 2 - ver alunos | 3 - atualizar aluno | 4 - excluir aluno | 5 - sair")
+    print('-' * 50)
+    op = int(input("qual opção vai escolher?: "))
+    print('-' * 50)
+
+    if op == 1: cadastrar_aluno()
+    elif op == 2: listar_aluno()
+    elif op == 3: atualizar_aluno()
+    elif op == 4: excluir_aluno()
+    elif op == 5:
+        print("encerrando sistema...")
+        break
