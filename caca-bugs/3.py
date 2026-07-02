cb77e1a6331199e80a3ad0337dfc499bd1c9f56a
igor <igorrocha2511@gmail.com>
2026-07-02 09:23:03 -0300

    02/07

--- caca-bugs/3.py
+++ caca-bugs/3.py
@@ -1,7 +1,52 @@
+#import sqlite3
+
+#def criar_tabelas():
+    #conexao = sqlite3.connect('sistema_escola.db')
+    #cursor = conexao.cursor()
+
+    # Este bloco quebra ao rodar pela primeira vez em um banco limpo. por que?
+   # cursor.execute('''
+#CREATE TABLE IF NOT EXISTS series (
+                   #id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   #nome_serie TEXT,
+                   #id_escola INTEGER,
+                   #FOREIGN KEY (id_escola) REFERENCES escolas (id)
+                  # )
+                  # ''')
+    
+    #cursor.execute('''
+#CREATE TABLE IF NOT EXISTS escolas (
+                   #id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   #nome TEXT
+                   #)
+                  # ''')
+    
+   # conexao.commit()
+    #conexao.close()
+
+                # o código da erro por estar na ordem invertida de criação de tabelas. ordem correta:
+
 import sqlite3
 
 def criar_tabelas():
     conexao = sqlite3.connect('sistema_escola.db')
     cursor = conexao.cursor()
 
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS escolas (
+                   #id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   #nome TEXT
+                   )
+                   ''')
     
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS series (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   nome_serie TEXT,
+                   id_escola INTEGER,
+                   FOREIGN KEY (id_escola) REFERENCES escolas (id)
+                   )
+                   ''')
+    
+    conexao.commit()
+    conexao.close()
---
+++ caca-bugs/5.py
@@ -0,0 +1,6 @@
+'''import sqlite3
+
+def vincular_aluno_turma():
+    nome = input("nome aluno: )
+    #se o usuario digitar "Turma B" em vez do numero do ID, o sistema quebra.
+    '''
